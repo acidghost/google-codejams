359e923f60453a58190b72db59ad83f4a04be94d
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-11 09:33:20 +0100

    done find items hs function

--- store-credit/store-credit.hs
+++ store-credit/store-credit.hs
@@ -36,9 +36,14 @@ doCase inh caseN = do
 	let listSize = read line :: Int
 	line <- hGetLine inh
 	let list = map (\e -> read e :: Int) (splitOn " " line)
-	let items = findItems list credits
-	putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ 
-		(show $ fst items) ++ " " ++ (show $ snd items)
+	case findItems (zip [1..] list) (zip [1..] list) credits of
+		Just items -> putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ 
+				(show $ fst items) ++ " " ++ (show $ snd items)
+		Nothing -> putStrLn "Nothing found..."
 
-findItems :: [Int] -> Int -> (Int, Int)
-findItems list credits = undefined
+findItems :: [(Int, Int)] -> [(Int, Int)] -> Int -> Maybe (Int, Int)
+findItems [] [] _ = Nothing
+findItems x@((xa, xb):xs) ((ya, yb):ys) credits
+	| xb + yb == credits = Just (xa, ya)
+	| length ys == 0 = findItems xs (tail xs) credits
+	| otherwise = findItems x ys credits
