db6c4a03115d8b2a358a3d2b6b694c05d7fc65d2
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-11 22:28:57 +0100

    configured hs for os x

--- store-credit/store-credit.hs
+++ store-credit/store-credit.hs
@@ -6,44 +6,44 @@ import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
-	args <- getArgs
-	if length args == 1 then
-		doFile (head args)
-	else do
-		progName <- getProgName
-		putStrLn $ "Usage: " ++ progName ++ " input.txt"
+    args <- getArgs
+    if length args == 1 then
+        doFile (head args)
+    else do
+        progName <- getProgName
+        putStrLn $ "Usage: " ++ progName ++ " input.txt"
 
 
 doFile :: String -> IO ()
 doFile fileName = do
-	inh <- openFile fileName ReadMode
-	line <- hGetLine inh
-	doCases inh 1 (read line :: Int)
+    inh <- openFile fileName ReadMode
+    line <- hGetLine inh
+    doCases inh 1 (read line :: Int)
 
 doCases :: Handle -> Int -> Int -> IO ()
 doCases inh caseN caseM = do
-	if caseN > caseM then
-		return ()
-	else do
-		doCase inh caseN
-		doCases inh (caseN + 1) caseM
+    if caseN > caseM then
+        return ()
+    else do
+        doCase inh caseN
+        doCases inh (caseN + 1) caseM
 
 doCase :: Handle -> Int -> IO ()
 doCase inh caseN = do
-	line <- hGetLine inh
-	let credits = read line :: Int
-	line <- hGetLine inh
-	let listSize = read line :: Int
-	line <- hGetLine inh
-	let list = map (\e -> read e :: Int) (splitOn " " line)
-	case findItems (zip [1..] list) (zip [1..] list) credits of
-		Just items -> putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ 
-				(show $ fst items) ++ " " ++ (show $ snd items)
-		Nothing -> putStrLn "Nothing found..."
+    line <- hGetLine inh
+    let credits = read line :: Int
+    line <- hGetLine inh
+    let listSize = read line :: Int
+    line <- hGetLine inh
+    let list = map (\e -> read e :: Int) (splitOn " " line)
+    case findItems (zip [1..] list) (zip [1..] list) credits of
+        Just items -> putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ 
+                (show $ fst items) ++ " " ++ (show $ snd items)
+        Nothing -> putStrLn "Nothing found..."
 
 findItems :: [(Int, Int)] -> [(Int, Int)] -> Int -> Maybe (Int, Int)
 findItems [] [] _ = Nothing
 findItems x@((xa, xb):xs) ((ya, yb):ys) credits
-	| xb + yb == credits = Just (xa, ya)
-	| length ys == 0 = findItems xs (tail xs) credits
-	| otherwise = findItems x ys credits
+    | xb + yb == credits = Just (xa, ya)
+    | length ys == 0 = findItems xs (tail xs) credits
+    | otherwise = findItems x ys credits
