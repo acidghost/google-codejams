ff306506680fbb4bb8ae386253fb839b414e6762
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-12 08:11:12 +0100

    reverse words in haskell

--- reverse-words/reverse-words.hs
+++ reverse-words/reverse-words.hs
@@ -1,8 +1,35 @@
 module Main where
+
 import System.Environment (getProgName, getArgs)
 import System.IO
-import Data.List.Split (splitOn)
+import Data.List (intercalate)
 
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    args <- getArgs
+    if length args == 1 then
+        doFile (head args)
+    else do
+        progName <- getProgName
+        putStrLn $ "Usage: " ++ progName ++ " input.txt"
+
+doFile :: String -> IO ()
+doFile fileName = do
+    inh <- openFile fileName ReadMode
+    line <- hGetLine inh
+    doCases inh 1 (read line :: Int)
+
+doCases :: Handle -> Int -> Int -> IO ()
+doCases inh caseN caseM = do
+    if caseN > caseM then
+        return ()
+    else do
+        doCase inh caseN
+        doCases inh (caseN + 1) caseM
+
+doCase :: Handle -> Int -> IO ()
+doCase inh caseN = do
+    line <- hGetLine inh
+    let list = intercalate " " $ reverse . words $ line
+    putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ list
