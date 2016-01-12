15304d5ce468f8333c61cf550046474683497bf4
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-12 20:02:33 +0100

    T9 spelling haskell solution

---
+++ t9-spelling/t9-spelling.hs
@@ -0,0 +1,69 @@
+module Main where
+
+import System.Environment (getProgName, getArgs)
+import System.IO
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if length args == 1 then
+        doFile (head args)
+    else do
+        progName <- getProgName
+        putStrLn $ "Usage: " ++ progName ++ " input.txt"
+
+doFile :: String -> IO ()
+doFile fileName = do
+    inh <- openFile fileName ReadMode
+    line <- hGetLine inh
+    doCases inh 1 (read line :: Int)
+
+doCases :: Handle -> Int -> Int -> IO ()
+doCases inh caseN caseM = do
+    if caseN > caseM then
+        return ()
+    else do
+        doCase inh caseN
+        doCases inh (caseN + 1) caseM
+
+doCase :: Handle -> Int -> IO ()
+doCase inh caseN = do
+    line <- hGetLine inh
+    putStrLn $ "Case #" ++ (show caseN) ++ ": " ++ t9Msg line
+
+t9Msg :: String -> String
+t9Msg = foldl t9CharFold []
+
+t9CharFold :: String -> Char -> String
+t9CharFold acc char
+    | acc == [] || last acc == head translated = acc ++ (' ':translated)
+    | otherwise = acc ++ translated
+    where translated = case char of 'a' -> "2"
+                                    'b' -> "22"
+                                    'c' -> "222"
+                                    'd' -> "3"
+                                    'e' -> "33"
+                                    'f' -> "333"
+                                    'g' -> "4"
+                                    'h' -> "44"
+                                    'i' -> "444"
+                                    'j' -> "5"
+                                    'k' -> "55"
+                                    'l' -> "555"
+                                    'm' -> "6"
+                                    'n' -> "66"
+                                    'o' -> "666";
+                                    'p' -> "7"
+                                    'q' -> "77"
+                                    'r' -> "777"
+                                    's' -> "7777"
+                                    't' -> "8"
+                                    'u' -> "88"
+                                    'v' -> "888"
+                                    'w' -> "9"
+                                    'x' -> "99"
+                                    'y' -> "999"
+                                    'z' -> "9999"
+                                    ' ' -> "0"
+
